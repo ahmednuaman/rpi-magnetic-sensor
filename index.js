8927f56d8859dfe4604cf41c77973588f4bef5ea
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-11-07 20:28:02 +0000

    adding messaging

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-import rpio from 'rpio'
-
-const PIN = 12
-
-rpio.open(PIN, rpio.INPUT, rpio.PULL_DOWN)
-rpio.poll(PIN, (pin) => {
-  const state = rpio.read(pin)
-
-  console.log(state)
-})
-
-setInterval(() => {
-  console.log(rpio.read(PIN))
-}, 200)
+import './lib/gpio'
